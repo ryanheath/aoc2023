74e39de02955e037f239da40d0e8ba0b7579d30d
Ryan Heath <ryan.q.heath@gmail.com>
2023-12-01 15:07:32 +0100

    day 1

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Buffers;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+
+Console.WriteLine("Start");
+
+// static void Day()
+// {
+//     ComputeExample(); Compute();
+
+//     void ComputeExample()
+//     {
+//         var input = @"".ToLines();
+//         Part1(input).Should().Be(0);
+//         Part2(input).Should().Be(0);
+//     }
+
+//     void Compute()
+//     {
+//         var input = File.ReadAllLines(".txt");
+//         Part1(input).Should().Be(0);
+//         Part2(input).Should().Be(0);
+//     }
+
+//     int Part1(string[] lines)
+//     {
+//         return 0;
+//     }
+
+//     int Part2(string[] lines)
+//     {
+//         return 0;
+//     }
+// }
+// Day();
+
+static void Day1()
+{
+    ComputeExample(); Compute();
+
+    void ComputeExample()
+    {
+        var input = @"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet".ToLines();
+        Part1(input).Should().Be(142);
+
+        input = @"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen".ToLines();
+        Part2(input).Should().Be(281);
+    }
+
+    void Compute()
+    {
+        var input = File.ReadAllLines("input1.txt");
+        Part1(input).Should().Be(55208);
+        Part2(input).Should().Be(54578);
+    }
+
+    int Part1(string[] lines) => lines.Select(Calibration).Sum();
+
+    int Part2(string[] lines) => lines.Select(CalibrationRegex).Sum();
+
+    static int Calibration(string line)
+    {
+        var sv = SearchValues.Create("123456789");
+        var span = line.AsSpan();
+        var fi = span.IndexOfAny(sv);
+        var li = span.LastIndexOfAny(sv);
+        var d = ToDigit(line[fi]) * 10 + ToDigit(line[li]);
+        return d;
+
+        static int ToDigit(char c) => c - '0';
+    }
+
+    static int CalibrationRegex(string line)
+    {
+        var regexpr = "one|two|three|four|five|six|seven|eight|nine";
+        var fsv = new Regex("[1-9]|" + regexpr);
+        var lsv = new Regex("[1-9]|" + regexpr.ReverseString());
+        return 
+            ToDigit(fsv.Match(line).Value) * 10 + 
+            ToDigit(lsv.Match(line.ReverseString()).Value.ReverseString());
+
+        static int ToDigit(string value) => value switch
+        {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            _ => value[0] - '0'
+        };
+    }
+}
+
+Day1();
+
+Console.WriteLine("Done!");
---
+++ StringExtensions.cs
@@ -0,0 +1,90 @@
+static class StringExtensions
+{
+    static public IEnumerable<List<string>> GroupLines(this string [] lines)
+    {
+        var group = new List<string>();
+
+        foreach(var line in lines)
+        {
+            if (line == "")
+            {
+                yield return group;
+                group = new List<string>();
+            }
+            else
+            {
+                group.Add(line);
+            }
+        }
+
+        yield return group;
+    }
+
+    static public string[] ToLines(this string input)
+    {
+        return input.Split("\r\n");
+    }
+
+    static public int[] ToInts(this string input)
+    {
+        return input.ToInts("\r\n");
+    }
+
+    static public int[] ToInts(this string input, string splitter)
+    {
+        return input.Split(splitter, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    }
+
+    static public long[] ToLongs(this string input, string splitter)
+    {
+        return input.Split(splitter, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+    }
+    static public ulong[] ToULongs(this string input, string splitter)
+    {
+        return input.Split(splitter, StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray();
+    }
+
+    static public int[] ToInts(this string[] input)
+    {
+        return input.Select(int.Parse).ToArray();
+    }
+
+    static public string Sort(this string input)
+    {
+        return string.Concat(input.OrderBy(c => c));
+    }
+
+    static public int ToInt(this string input)
+    {
+        return int.Parse(input);
+    }
+
+    static public string ToBits(this int input)
+    {
+        return Convert.ToString(input, 2);
+    }
+
+    static public string ToBits(this byte input)
+    {
+        return Convert.ToString(input, 2);
+    }
+
+    static public int IntFromBits(this string input) => input.AsSpan().IntFromBits();
+
+    static public int IntFromBits(this ReadOnlySpan<char> input)
+    {
+        var v = 0;
+        foreach (var c in input)
+        {
+            v <<= 1;
+            if (c == '1')
+            {
+                v |= 1;
+            }
+        }
+
+        return v;
+    }
+
+    public static string ReverseString(this string s) => new(s.Reverse().ToArray());
+}
