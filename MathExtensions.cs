c3c0a81f437bfa7ed943db9024bca398afc9103a
Ryan Heath <ryan.q.heath@gmail.com>
2023-12-09 19:08:24 +0100

    generalized math extensions

--- MathExtensions.cs
+++ MathExtensions.cs
@@ -1,15 +1,15 @@
 static class MathExtensions
 {
-    public static long Gcd(long a, long b)
+    public static T Gcd<T>(T a, T b) where T : INumber<T>
     {
-        while (b != 0) (b, a) = (a % b, b);
+        while (b != T.Zero) (b, a) = (a % b, b);
 
         return a;
     }
 
-    public static long Lcm(long a, long b) => b / Gcd(a, b) * a;
+    public static T Lcm<T>(T a, T b) where T : INumber<T> => b / Gcd(a, b) * a;
 
-    public static long Gcd(this IEnumerable<long> numbers) => numbers.Aggregate(Gcd);
+    public static T Gcd<T>(this IEnumerable<T> numbers) where T : INumber<T> => numbers.Aggregate(Gcd);
 
-    public static long Lcm(this IEnumerable<long> numbers) => numbers.Aggregate(Lcm);
+    public static T Lcm<T>(this IEnumerable<T> numbers) where T : INumber<T>  => numbers.Aggregate(Lcm);
 }
--- global-usings.cs
+++ global-usings.cs
@@ -1,5 +1,6 @@
-﻿global using System.Diagnostics;
+﻿global using System.Buffers;
+global using System.Diagnostics;
+global using System.Numerics;
 global using System.Reflection;
-global using System.Buffers;
 global using System.Text.RegularExpressions;
 global using FluentAssertions;
