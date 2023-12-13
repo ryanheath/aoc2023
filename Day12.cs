61311e8885351fee3110f4d7d3460fd496ce99b8
Ryan Heath <ryan.q.heath@gmail.com>
2023-12-13 21:59:30 +0100

    Day 12, part 1

---
+++ Day12.cs
@@ -0,0 +1,124 @@
+﻿static partial class Aoc2023
+{
+    public static void Day12()
+    {
+        var day = MethodBase.GetCurrentMethod()!.Name;
+        Console.WriteLine(day);
+
+        ComputeExample(); Compute();
+
+        void ComputeExample()
+        {
+            var input = 
+                """
+                ???.### 1,1,3
+                .??..??...?##. 1,1,3
+                ?#?#?#?#?#?#?#? 1,3,1,6
+                ????.#...#... 4,1,1
+                ????.######..#####. 1,6,5
+                ?###???????? 3,2,1
+                """.ToLines();
+            Part1(input).Should().Be(21);
+
+            input = 
+                """
+                abc
+                """.ToLines();
+            Part2(input).Should().Be(0);
+        }
+
+        void Compute()
+        {
+            var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
+            Part1(input).Should().Be(6935);
+            Part2(input).Should().Be(0);
+        }
+
+        int Part1(string[] lines) => lines.Select(ParseSpring).Select(CountArrangements).Sum();
+
+        int Part2(string[] lines) => 0;
+
+        (char[], int[]) ParseSpring(string line)
+        {
+            var parts = line.Split(" ");
+            var conditions = parts[0].ToCharArray();
+            var broken = parts[1].ToInts(",");
+            return (conditions, broken);
+        }
+
+        int CountArrangements((char[], int[]) spring)
+        {
+            var countArrangements = 0;
+            var (conditions, broken) = spring;
+            
+            var possibleOffsets = conditions.Select((c, i) => (c, i)).Where(x => x.c != '.').Select(x => x.i).ToList();
+
+            var work = new List<(int brokenIndex, int offset, int count)>();
+            
+            // enqueue all possible offsets for first broken
+            EnqueueWork(0, 0, 0, 1);
+
+            while (work.Count > 0)
+            {
+                var (brokenIndex, offset, count) = work[0]; work.RemoveAt(0);
+
+                var brokenLength = broken[brokenIndex];
+                var offsetEnd = offset + brokenLength;
+
+                // check if we can place the broken here
+                if (!((offset == 0 || conditions[offset - 1] != '#')
+                    && (offsetEnd == conditions.Length || offsetEnd < conditions.Length && conditions[offsetEnd] != '#')
+                    && !conditions.AsSpan(offset, brokenLength).Contains('.')))
+                {
+                    continue;
+                }
+
+                // did we reach the end?
+                if (brokenIndex == broken.Length - 1)
+                {
+                    // if there are # left, we did not find a solution
+                    if (!conditions.AsSpan(offsetEnd).Contains('#'))
+                    {
+                        countArrangements += count;
+                    }
+                }
+                else
+                {
+                    // find next possible that should fit
+                    var nextPossibleIndex = possibleOffsets.BinarySearch(offsetEnd+1);
+                    if (nextPossibleIndex < 0) nextPossibleIndex = ~nextPossibleIndex;
+                    
+                    // enqueue next possible offset for next brokens
+                    EnqueueWork(brokenIndex + 1, offsetEnd, nextPossibleIndex, count);
+                }
+            }
+
+            return countArrangements;
+
+            void EnqueueWork(int bi, int offsetEnd, int nextPossibleIndex, int count)
+            {
+                for (var i = nextPossibleIndex; i < possibleOffsets.Count; i++)
+                {
+                    var nextOffset = possibleOffsets[i];
+
+                    // if we find # in between, we must stop
+                    if (conditions.AsSpan(offsetEnd, nextOffset - offsetEnd).Contains('#')) 
+                    {
+                        break;
+                    }
+
+                    // find duplicates
+                    var wi = work.FindIndex(w => w.brokenIndex == bi && w.offset == nextOffset);
+                    if (wi >= 0)
+                    {
+                        work[wi] = work[wi] with { count = work[wi].count + count };
+                    }
+                    else
+                    {
+                        work.Add((bi, nextOffset, count));
+                    }
+                }
+            }
+        }
+    }
+}
