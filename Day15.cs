8b05fb8e7fd1107d8bf8de3a56abe8041142f65e
Ryan Heath <ryan.q.heath@gmail.com>
2023-12-15 10:42:03 +0100

    Day 15, part 1

--- Day15.cs
+++ Day15.cs
@@ -1,6 +1,7 @@
 ﻿static partial class Aoc2023
 {
-    public static void DayTemplate()
+    public static void Day15
+    ()
     {
         var day = MethodBase.GetCurrentMethod()!.Name;
         Console.WriteLine(day);
@@ -9,23 +10,26 @@
 
         void ComputeExample()
         {
+            HASH("HASH");
+
             var input = 
                 """
-                abd
+                rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
                 """.ToLines();
-            Part1(input).Should().Be(0);
+            Part1(input).Should().Be(1320);
             Part2(input).Should().Be(0);
         }
 
         void Compute()
         {
             var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
-            Part1(input).Should().Be(0);
+            Part1(input).Should().Be(510388);
             Part2(input).Should().Be(0);
         }
 
-        int Part1(string[] lines) => 0;
-
+        int Part1(string[] lines) => lines.Select(l => l.Split(',')).SelectMany(l => l).Select(HASH).Sum();
         int Part2(string[] lines) => 0;
+
+        int HASH(string s) => s.Aggregate(0, (h, c) => (h + c) * 17 % 256);
     }
 }
