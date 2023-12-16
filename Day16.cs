6bd64e8dcb9434b3aa39381a9e16d66909398444
Ryan Heath <ryan.q.heath@gmail.com>
2023-12-16 14:33:20 +0100

    Day 16, refactor

--- Day16.cs
+++ Day16.cs
@@ -33,10 +33,10 @@
             Part2(input).Should().Be(8185);
         }
 
-        int Part1(string[] lines) => CountEnergized(TraverseTiles(ParseTiles(lines), lines.Length, lines[0].Length));
-        int Part2(string[] lines) => CountMaxEnergized(ParseTiles(lines), lines.Length, lines[0].Length);
+        int Part1(string[] lines) => CountEnergized(TraverseTiles(ParseMirrors(lines), lines.Length, lines[0].Length));
+        int Part2(string[] lines) => CountMaxEnergized(ParseMirrors(lines), lines.Length, lines[0].Length);
 
-        static int CountMaxEnergized(Dictionary<(int y, int x), MirrorTile> tiles, int maxY, int maxX)
+        static int CountMaxEnergized(Dictionary<(int y, int x), char> mirrors, int maxY, int maxX)
         {
             var max = 0;
             for (var x = 0; x < maxX; x++)
@@ -52,13 +52,14 @@
             return max;
 
             void FindMaxEnergized(int y, int x, Direction direction) 
-                => max = Math.Max(max, CountEnergized(TraverseTiles(DeepCopy(tiles), maxY, maxX, y, x, direction)));
+                => max = Math.Max(max, CountEnergized(TraverseTiles(mirrors, maxY, maxX, y, x, direction)));
         }
 
-        static Dictionary<(int y, int x), MirrorTile> TraverseTiles(Dictionary<(int y, int x), MirrorTile> tiles, 
+        static Dictionary<(int y, int x), List<Direction>> TraverseTiles(Dictionary<(int y, int x), char> mirrors, 
             int maxY, int maxX,
             int startY = 0, int startX = 0, Direction startDirection = Direction.E)
         {
+            var tiles = new Dictionary<(int y, int x), List<Direction>>();
             var beams = new Queue<(int y, int x, Direction direction)>();
             beams.Enqueue((startY, startX, startDirection));
 
@@ -66,72 +67,65 @@
             {
                 var (y, x, direction) = beams.Dequeue();
 
+                var mirror = mirrors.GetValueOrDefault((y, x), '.');
+
                 if (!tiles.TryGetValue((y, x), out var tile))
+                    tiles[(y, x)] = tile = [];
+
+                // if beam already exists, skip
+                if (tile.Contains(direction)) continue;
+
+                // add beam to mirror tile
+                tile.Add(direction);
+
+                switch (mirror)
                 {
-                    // add same direction beam to empty tile
-                    tiles[(y, x)] = new MirrorTile('.', [direction]);
-                    ContinueBeam(y, x, direction);
+                    case '/':
+                        direction = direction switch
+                        {
+                            Direction.N => Direction.E,
+                            Direction.E => Direction.N,
+                            Direction.S => Direction.W,
+                            Direction.W => Direction.S,
+                            _ => direction
+                        };
+                        break;
+
+                    case '\\':
+                        direction = direction switch
+                        {
+                            Direction.N => Direction.W,
+                            Direction.E => Direction.S,
+                            Direction.S => Direction.E,
+                            Direction.W => Direction.N,
+                            _ => direction
+                        };
+                        break;
+
+                    case '-':
+                        switch (direction)
+                        {
+                            case Direction.N:
+                            case Direction.S:
+                                ContinueBeam(y, x, Direction.W);
+                                ContinueBeam(y, x, Direction.E);
+                                continue; // skip to next beam
+                        }
+                        break;
+
+                    case '|':
+                        switch (direction)
+                        {
+                            case Direction.E:
+                            case Direction.W:
+                                ContinueBeam(y, x, Direction.N);
+                                ContinueBeam(y, x, Direction.S);
+                                continue; // skip to next beam
+                        }
+                        break;
                 }
-                else 
-                {
-                    // if beam already exists, skip
-                    if (tile.Beams.Contains(direction)) continue;
 
-                    // add beam to mirror tile
-                    tile.Beams.Add(direction);
-
-                    switch (tile.Mirror)
-                    {
-                        case '/':
-                            var newDirection = direction switch
-                            {
-                                Direction.N => Direction.E,
-                                Direction.E => Direction.N,
-                                Direction.S => Direction.W,
-                                Direction.W => Direction.S,
-                                _ => direction
-                            };
-                            ContinueBeam(y, x, newDirection);
-                            break;
-
-                        case '\\':
-                            newDirection = direction switch
-                            {
-                                Direction.N => Direction.W,
-                                Direction.E => Direction.S,
-                                Direction.S => Direction.E,
-                                Direction.W => Direction.N,
-                                _ => direction
-                            };
-                            ContinueBeam(y, x, newDirection);
-                            break;
-
-                        case '-':
-                            switch (direction)
-                            {
-                                case Direction.N:
-                                case Direction.S:
-                                    ContinueBeam(y, x, Direction.W);
-                                    ContinueBeam(y, x, Direction.E);
-                                    break;
-                                default: ContinueBeam(y, x, direction); break;
-                            }
-                            break;
-                        case '|':
-                            switch (direction)
-                            {
-                                case Direction.E:
-                                case Direction.W:
-                                    ContinueBeam(y, x, Direction.N);
-                                    ContinueBeam(y, x, Direction.S);
-                                    break;
-                                default: ContinueBeam(y, x, direction); break;
-                            }
-                            break;
-                    
-                        default: ContinueBeam(y, x, direction); break;
-                    }
-                }
+                ContinueBeam(y, x, direction);
             }
 
             return tiles;
@@ -151,26 +145,16 @@
             }   
         }
 
-        static int CountEnergized(Dictionary<(int y, int x), MirrorTile> tiles) 
-            => tiles.Values.Count(t => t.Beams.Count > 0);
+        static int CountEnergized(Dictionary<(int y, int x), List<Direction>> tiles) 
+            => tiles.Values.Count(t => t.Count > 0);
 
-        static Dictionary<(int y, int x), MirrorTile> ParseTiles(string[] lines)
+        static Dictionary<(int y, int x), char> ParseMirrors(string[] lines)
         {
-            var tiles = new Dictionary<(int y, int x), MirrorTile>();
+            var tiles = new Dictionary<(int y, int x), char>();
             for (var y = 0; y < lines.Length; y++)
                 for (var x = 0; x < lines[y].Length; x++)
-                    if (lines[y][x] != '.') tiles[(y, x)] = new MirrorTile(lines[y][x], []);
+                    if (lines[y][x] != '.') tiles[(y, x)] = lines[y][x];
             return tiles;
         }
-
-        static Dictionary<(int y, int x), MirrorTile> DeepCopy(Dictionary<(int y, int x), MirrorTile> tiles)
-        {
-            var copy = new Dictionary<(int y, int x), MirrorTile>();
-            foreach (var (key, value) in tiles)
-                copy[key] = new (value.Mirror, [..value.Beams]);
-            return copy;
-        }
     }
-
-    record MirrorTile(char Mirror, List<Direction> Beams);
 }
