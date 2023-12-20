9ee3082c016d5d3b3fc4a315ed1584b7fe610309
Ryan Heath <ryan.q.heath@gmail.com>
2023-12-20 18:11:14 +0100

    Day 20, part 2

--- Day20.cs
+++ Day20.cs
@@ -26,20 +26,43 @@
                 &con -> output
                 """.ToLines();
             Part1(input).Should().Be(11687500);
-            Part2(input).Should().Be(0);
         }
 
         void Compute()
         {
             var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
             Part1(input).Should().Be(812609846);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(245114020323037);
         }
 
-        int Part1(string[] lines) => PushButton(1000, ParseModules(lines));
-        int Part2(string[] lines) => 0;
+        int Part1(string[] lines) => PushButtonTimes(1000, ParseModules(lines));
+        long Part2(string[] lines) => DebugPushButton("rx", ParseModules(lines));
 
-        static int PushButton(int times, Dictionary<string, ModuleBase> modules)
+        static long DebugPushButton(string trackOutput, Dictionary<string, ModuleBase> modules)
+        {
+            // skip first parent modules
+            var inputsForTrackOutput1 = modules.Values.Where(x => x.Outputs.Contains(trackOutput));
+            // use second parent modules
+            ModuleBase[] inputsForTrackOutput2 = [..modules.Values.Where(x => x.Outputs.Any(y => inputsForTrackOutput1.Any(z => z.Name == y)))];
+
+            var counts = new Dictionary<string, long>();
+
+            for (var times = 1; ; times++)
+            {
+                PushButton(modules);
+
+                foreach (var conjunction in inputsForTrackOutput2.OfType<ConjunctionModule>())
+                    if (conjunction.PulseHighCount == 1 && !counts.ContainsKey(conjunction.Name))
+                        counts[conjunction.Name] = times;
+
+                if (counts.Count == inputsForTrackOutput2.Length)
+                    break;
+            }
+
+            return counts.Values.Lcm();
+        }
+
+        static int PushButtonTimes(int times, Dictionary<string, ModuleBase> modules)
         {
             for (var i = 0; i < times; i++)
                 PushButton(modules);
@@ -48,26 +71,27 @@
             var totalHigh = modules.Values.Sum(x => x.PulseHighCount);
 
             return totalLow * totalHigh;
+        }
 
-            static void PushButton(Dictionary<string, ModuleBase> modules)
+        static void PushButton(Dictionary<string, ModuleBase> modules)
+        {
+            var output = new OutputModule();
+            var bus = new Queue<(string sender, string receiver, Pulse)>();
+            bus.Enqueue(("button", modules["button"].Outputs[0], Pulse.Low));
+
+            while (bus.Count > 0)
             {
-                var bus = new Queue<(string sender, string receiver, Pulse)>();
-                bus.Enqueue(("button", modules["button"].Outputs[0], Pulse.Low));
+                var (senderName, receiverName, pulse) = bus.Dequeue();
+                var sender = modules[senderName];
+                if (!modules.TryGetValue(receiverName, out var receiver))
+                    receiver = output;
 
-                while (bus.Count > 0)
-                {
-                    var (senderName, receiverName, pulse) = bus.Dequeue();
-                    var sender = modules[senderName];
-                    if (!modules.TryGetValue(receiverName, out var receiver))
-                        receiver = modules["output"];
+                var result = sender.SendPulse(receiver, pulse);
 
-                    var result = sender.SendPulse(receiver, pulse);
+                if (result == Pulse.None) continue;
 
-                    if (result == Pulse.None) continue;
-
-                    foreach (var outputName in receiver.Outputs)
-                        bus.Enqueue((receiverName, outputName, result));
-                }
+                foreach (var outputName in receiver.Outputs)
+                    bus.Enqueue((receiverName, outputName, result));
             }
         }
 
@@ -76,7 +100,6 @@
             var modules = new Dictionary<string, ModuleBase>(lines.Select(ParseModule));
 
             modules["button"] = new ButtonModule();
-            modules["output"] = new OutputModule();
 
             // register inputs for conjunction modules
             foreach (var module in modules.Values.OfType<ConjunctionModule>())
@@ -115,7 +138,7 @@
         public int PulseHighCount { get; private set; }
         public int PulseLowCount { get; private set; }
 
-        virtual public Pulse SendPulse(ModuleBase receiver, Pulse pulse)
+        public Pulse SendPulse(ModuleBase receiver, Pulse pulse)
         {
             if (pulse == Pulse.High) PulseHighCount++;
             if (pulse == Pulse.Low) PulseLowCount++;
@@ -127,7 +150,6 @@
 
     record class OutputModule() : ModuleBase("output", [])
     {
-        virtual public void SendPulse(Pulse pulse) {}
         override protected Pulse ReceivePulse(ModuleBase sender, Pulse pulse) => Pulse.None;
     }
 
